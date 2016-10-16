4b956386312b778d5a9cc8eb1b3268dbf864fa49
Subhendu Bhatta <bhattasubhendu@gmail.com>
2016-10-16 16:01:21 +0545

    laramap live

---
+++ resources/views/front.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="container text-center">
+
+	<h2>Laramap Live on Heroku</h2>
+
+	<div id="map">
+
+	</div>
+
+</div>
+
+
+@endsection
